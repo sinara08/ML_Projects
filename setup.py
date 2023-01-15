50942822c338681a95172d39a7c1066d7b88d32d
sinara08 <109323780+sinara08@users.noreply.github.com>
2023-01-15 01:40:08 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     name="sensor",
     version="0.0.1",
     author="ineuron",
-    author_email="avnish@ineuron.ai",
+    author_email="sinara.s@gmail.com",
     packages = find_packages(),
     install_requires=get_requirements(),#["pymongo==4.2.0"],
 )
